83268ba6ff2579fcb3444c73f3a6512cefd1b9bd
ImranTheMan <ijason2002@gmail.com>
2024-04-15 15:57:26 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -3,86 +3,158 @@ import { useState } from "react";
 const tempMusicData = [
   {
     id: 1,
-    title: "Song 1",
-    artist: "Artist A",
+    title: "Pantropiko",
+    artist: "Bini",
     genre: "Pop",
   },
   {
     id: 2,
-    title: "Song 2",
-    artist: "Artist B",
-    genre: "Rock",
+    title: "Alam mo ba girl?",
+    artist: "Hev Abi",
+    genre: "HipHop",
   },
   {
     id: 3,
-    title: "Song 3",
-    artist: "Artist C",
-    genre: "Jazz",
+    title: "Selos",
+    artist: "Shaira",
+    genre: "Pop",
   },
 ];
 const tempPlaylist = [
   {
     id: 1,
-    title: "Song 1",
-    artist: "Artist A",
-    genre: "Pop",
+    title: "Neneng B",
+    artist: "Nik Makino",
+    genre: "Rap",
+    userRating: 5
   },
   {
     id: 2,
-    title: "Song 2",
-    artist: "Artist B",
-    genre: "Rock",
+    title: "Babaero",
+    artist: "Hev Abi",
+    genre: "HipHop",
+    userRating: 4
   },
-  {
-    id: 3,
-    title: "Song 3",
-    artist: "Artist C",
-    genre: "Jazz",
-  },
+  
 ];
-function App() {
-  const [query, setQuery] = useState("");
-  const [musics, setMusic] = useState(tempMusicData);
+
+function Box({children, title}){
+  return (
+    <div className="container">
+      <h2>{title}</h2>{children}</div>
+  )
+}
+// function MusicListBox({music}){
+//   return(
+//     <div className="container">
+//       <h2>Music List</h2>
+//       <Music music={music}/>
+//   </div>
+//   );
+// }
+// function PlaylistBox(){
+//   return(
+//     <div className="container">
+//     <h2>Play List</h2>
+//     <PlayList/>
+//   </div>
+//   );
+// }
+
+
+function PlayList(){
   const [playlist, setPlaylist] = useState(tempPlaylist);
   const addToPlaylist = (music) => {
     setPlaylist([...playlist, music]);
-  };
-  return (
+  }
+  return(
+    <ul>
+      {playlist.map((music) => (
+        <li key={music.id}>
+          {music.title} by {music.artist}
+          <p>
+            <span>⭐</span>
+            <span>3</span>
+          </p>
+        </li>
+      ))}
+    </ul> 
+  );
+}
+function Music({music}){
+  return(
     <div>
-      <h1 style={{ textAlign: "center" }}>Music App</h1>
-      <input
-        className="search"
-        type="text"
-        placeholder="Search movies..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <div className="container">
-        <div className="container">
-          <h2>Music List</h2>
-
-          <ul>
-            {musics.map((music) => (
-              <li key={music.id}>
-                {music.title} by {music.artist} ({music.genre})
-                <button>♥️</button>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="container">
-          <h2>Playlist</h2>
-          <ul>
-            {playlist.map((music) => (
-              <li key={music.id}>
-                {music.title} by {music.artist}
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
+    <ul>
+      {music.map((music) => (
+        <li key={music.id}>
+          {music.title} by {music.artist} ({music.genre})<button>♥️</button>
+        </li>
+      ))}
+    </ul>
     </div>
   );
 }
+function NavBar({children}){
+  return(  
+  <nav className="container">
+
+        {children}
+  </nav>
+  );
+}
+function Logo(){
+  return <h1 style={{ textAlign: "center" }}>Music App</h1>
+}
+function NumResult({music}){
+  return(
+    <p>
+          Found <strong>{music.length}</strong> results
+    </p>
+  );
+  
+}
+function Search(){
+  const [query, setQuery] = useState("");
+  return(
+  <input
+          className="search"
+          type="text"
+          placeholder="Search movies..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+    />
+  );
+}
+function Main({children}){
+  return(
+    <div className="container">
+      {children}
+      {/* <MusicListBox music={music}/>
+      <PlaylistBox/> */}
+    </div>
+  );
+}
+function App() {
+  const [music, setMusic] = useState(tempMusicData);
+  return (
+    <>
+      <NavBar>
+        <Logo/>
+        <Search/>
+        <NumResult music={music}/>
+      </NavBar>
+      <Main>
+        <Box title="Music List">
+          <Music music={music}/>
+        </Box>
+        <Box title ="Playlist">
+          <PlayList/>
+        </Box>
+        <Box title="ETC list"></Box>
+        </Main>
+      </>
+  );
+}
+
 
 export default App;
